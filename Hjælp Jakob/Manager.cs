6d97182a020d30bf6e2ab9128b01a23cdb91f050
Tobias Goldkette Prang <tobi8268@zbc.dk>
2021-02-23 09:19:20 +0100

    Add project files.

---
+++ Hjælp Jakob/Manager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hjælp_Jakob
+{
+    class Manager
+    {
+
+        public void sendMessage(MessageCarrier type, string to, Message m, bool isHTML)
+        {
+            //herinde sendes der en email ud til modtageren
+            if (type.Equals(MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+
+        public void sendMessages(MessageCarrier type, string[] to, Message m, bool isHTML)
+        {
+            if (type.Equals(MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = HTMLConverter.ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+    }
+
+    public class HTMLConverter
+    {
+        public static string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+    }
+}
